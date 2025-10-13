41132923b21b4b847f08601f7d4e6697ac75d9a2
Martin Rodriguez <martinrodriguezdev96@gmail.com>
2025-10-13 10:55:41 -0300

    controllers creation

---
+++ src/tasks/task.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get } from "@nestjs/common";
+
+@Controller({})
+export class TaskController {
+    
+    @Get('/tasks')
+    getAllTasks(){
+        return 'Obteniendo todas las tareas'
+    }
+}

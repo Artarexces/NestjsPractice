59412d4397e12c6e83d8cd9209437a6f58de79ae
Martin Rodriguez <martinrodriguezdev96@gmail.com>
2025-10-13 13:23:59 -0300

    add: first service!

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { ProjectsModule } from './projects/projects.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { HellowController } from './hellow/hellow.controller';
+import { UsersService } from './users/users.service';
 
 @Module({
   imports: [TasksModule, ProjectsModule, AuthModule, UsersModule],
-  controllers: [HellowController]
+  controllers: [HellowController],
+  providers: [UsersService]
 })
 export class AppModule {}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
+import { UsersService } from './users.service';
+
 
 @Controller()
 export class UsersController {
+constructor(private readonly userService:UsersService){}
 
     @Get('users')
     getUsers(): string[]{
-        return['niggel', 'tino', 'axel']
+        return this.userService.getUsers();
     }
+
 }
